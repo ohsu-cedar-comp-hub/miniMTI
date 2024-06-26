32c0fb2e22a11c9102d6a364c08959e8f0bd3c55
zacsims <zacharyssims@gmail.com>
2024-06-26 12:37:46 -0700

    added updated eval scripts

--- eval/intensity.py
+++ eval/intensity.py
@@ -1,18 +1,14 @@
 import os
 import sys
 import gc
-from tqdm import tqdm
 import torch
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage.io import imread
-from skimage.transform import rescale
-from scipy.stats import gaussian_kde
+from tqdm import tqdm
 from einops import rearrange, repeat
-from torchmetrics.functional import structural_similarity_index_measure as ssim
-from torchmetrics.functional import spearman_corrcoef as spearman
+from torchmetrics.functional import structural_similarity_index_measure as ssim, spearman_corrcoef as spearman
 
-def get_mints(unmasked, gt, preds, mask, device):
+def get_mints(gt, preds, mask, device):
+    '''calculates mean intensities (mints) and predicted mean intensities (pmints'''
     #reshape to BxCxHxW
     preds = rearrange(preds, 'b c (h w) -> b c h w', h=32)
     gt = rearrange(gt, 'b c (h w) -> b c h w', h=32)
@@ -28,43 +24,9 @@ def get_mints(unmasked, gt, preds, mask, device):
     return mints, pmints
 
 
-
-def get_intensities_run_panel_order(model, panel, val_loader, max_panel_size, batch_size, device='cpu'):
-    
-    masked_ch_idx = torch.tensor([i for i in range(max_panel_size) if i not in panel], device=device)
-    model.mae.masking_ratio = (max_panel_size - len(panel)) / max_panel_size
-
-    with torch.no_grad():
-        #set tensors on cpu to store predicted and ground-truth mean intensities 
-        mints= torch.zeros((len(val_loader.dataset), len(masked_ch_idx)), device='cpu')
-        pmints = mints.clone()
-
-        #run each batch through the model and copy intensities back to cpu
-        for i, (ims, mask, filepaths) in enumerate(tqdm(val_loader)):
-            ims = ims.to(device)
-            panel_patches, gt, preds,_,_ = model.forward((ims, mask, filepaths), masked_patch_idx=masked_ch_idx)
-            mints_, pmints_ = get_mints(ims, gt, preds, mask, device)
-            s = i * batch_size
-            e = s + batch_size
-            mints_ = mints_.to('cpu')
-            pmints_ = pmints_.to('cpu')
-            mints[s:e, :] = mints_
-            pmints[s:e, :] = pmints_
-
-            del ims
-            if device != 'cpu':
-                gc.collect()
-                torch.cuda.empty_cache()
-                     
-    return mints, pmints
-
-
-def get_intensities(model, panel, val_loader, max_panel_size, calculate_ssims, include_he, save_pred, pred_img_dir_path, size, device='cpu'):
-
-    # if include_he: max_panel_size += 3
-    keep_channels, keep_channels_idx, ch2idx = get_channel_info(include_he=include_he)
-    ch2stain = {i:ch for ch,i in ch2idx.items()}
-
+def get_intensities(model, panel, val_loader, ch2stain, calculate_ssims=False, device='cpu'):
+    '''runs inference with model in batches on GPU and copies mean intensities and predicted mean intensities (and SSIM if specified) back to CPU'''
+    max_panel_size = len(ch2stain.keys())
     ordered_markers_to_impute = [ch2stain[idx] for idx in [i for i in range(max_panel_size) if i not in panel]]
     current_marker_panel = [ch2stain[idx] for idx in panel]
     masked_ch_idx_list = [i for i in range(max_panel_size) if i not in panel]
@@ -72,7 +34,7 @@ def get_intensities(model, panel, val_loader, max_panel_size, calculate_ssims, i
     panel_ch_idx = torch.tensor(panel, device=device)  # This is the panel list, in this above example, this will be tensor([0, 4, 12])
     model.mae.masking_ratio = (max_panel_size - len(panel)) / max_panel_size
 
-    print(f"*************** Processing {size} panel ***************")
+    print(f"*************** Processing {len(panel)} panel ***************")
     print("-- Current marker panel is:", current_marker_panel)
     print("-- Current panel marker index: ", panel)
     print("-- Markers to impute: ", ordered_markers_to_impute)
@@ -80,122 +42,60 @@ def get_intensities(model, panel, val_loader, max_panel_size, calculate_ssims, i
     print("-- Calculating SSIM: ", calculate_ssims)
     print("-- Current MAE masking ratio is: ", model.mae.masking_ratio)
     
+    #initiate empty arrays on CPU
+    mints = torch.zeros((len(val_loader.dataset), len(masked_ch_idx)), device='cpu')
+    pmints = mints.clone()
+    ssims = torch.zeros(len(val_loader.dataset), device='cpu') if calculate_ssims else None
 
-    with torch.no_grad():
-        # Initialize tensors to store results
-        # this will create a zero matrix of shape (dataset_size, masked_ch_idx_size), in the above example, this will be (50000, 14) 
-        mints = torch.zeros((len(val_loader.dataset), len(masked_ch_idx)), device='cpu')
-        pmints = mints.clone()
-        # set up the combined mint and pmint
-        # the additional 2 is for the centroid information
-        mints_combined = torch.zeros((len(val_loader.dataset), max_panel_size + 2), device='cpu')
-        pmints_combined = mints_combined.clone()
-        # set up the ssims
-        ssims = torch.zeros(len(val_loader.dataset), device='cpu') if calculate_ssims else None
+    print(f"-- Processing batches ......")
 
-        # creating list to store pred and gt images
-        all_preds = []
-        all_gt = []
-
-        print(f"-- Processing batches ......")
+    batch_size = None
+    for i, batch in enumerate(tqdm(val_loader)):
+        ims, masks, filepaths = batch
+        ims = ims.to(device)
         
-        batch_size = None
-        for i, batch in enumerate(tqdm(val_loader)):
-            ims, masks, filepaths = batch
-            # Process batch and mask 
-            # print(f"-- Processing batch {i} ......")
-            ims = ims.to(device)
-            if batch_size is None: 
-                batch_size = len(ims)
-
-            # get centroid information from filepaths
-            # centroids will be a list with a dimension of (batch_size, 2)
-            # print("-- Getting the centroid information......")
-            centroids = []
-            for filepath in filepaths:
-                parts = filepath.split('-')
-                x_part = parts[-2]
-                y_part = parts[-1]
-                x_centroid = int(x_part.split('=')[1])
-                y_centroid = int(y_part.split('=')[1].replace('.tif', ''))
-                centroids.append((x_centroid, y_centroid))
-            centroids_tensor = torch.tensor(centroids, device=device)
-        
+        #get batch size from first batch
+        if batch_size is None: 
+            batch_size = len(ims)
+            
+        #run inference on GPU
+        with torch.no_grad():
             panel_patches, gt, preds, _, _ = model.forward((ims, masks, filepaths), masked_patch_idx=masked_ch_idx)
-            concatenated_gt = torch.cat((panel_patches, gt), dim=1)
-            concatenated_preds = torch.cat((panel_patches, preds), dim=1)
 
-            # reshape gt and preds from torch.Size([batch_size, 16, 1024]) to torch.Size([batch_size, 16, 32, 32])
-            preds_reshape = rearrange(preds, 'b c (h w) -> b c h w', h=32)
-            gt_reshape = rearrange(gt, 'b c (h w) -> b c h w', h=32)
+        # reshape gt and preds from torch.Size([batch_size, 16, 1024]) to torch.Size([batch_size, 16, 32, 32])
+        preds_reshape = rearrange(preds, 'b c (h w) -> b c h w', h=32)
+        gt_reshape = rearrange(gt, 'b c (h w) -> b c h w', h=32)
 
-            mints_, pmints_ = get_mints(ims, gt, preds, masks, device) # without panel patches 
-            mints_concat_, pmints_concat_ = get_mints(ims, concatenated_gt, concatenated_preds, masks, device) # with panel patches
+        #get mean intensities
+        mints_, pmints_ = get_mints(gt, preds, masks, device) # without panel patches 
 
-            mints_combined_ = torch.cat((mints_concat_, centroids_tensor), dim=1)
-            pmints_combined_ = torch.cat((pmints_concat_, centroids_tensor), dim=1)
+        #copy back to CPU
+        s = i * batch_size
+        e = s + batch_size
+        mints[s:e, :] = mints_.to('cpu')
+        pmints[s:e, :] = pmints_.to('cpu')
 
-            s = i * batch_size
-            e = s + batch_size
+        # SSIM calculation if requested
+        if calculate_ssims:
+            preds_reshape[preds_reshape < 0] = 0
+            ssims_ = ssim(gt_reshape, preds_reshape, reduction='none')
+            ssims_ = ssims_.to('cpu')
+            ssims[s:e] = ssims_
 
-            mints[s:e, :] = mints_.to('cpu')
-            pmints[s:e, :] = pmints_.to('cpu')
+        #memory management
+        del batch
+        if device != 'cpu':
+            gc.collect()
+            torch.cuda.empty_cache()
 
-            mints_combined[s:e, :] = mints_combined_.to('cpu')
-            pmints_combined[s:e, :] = pmints_combined_.to('cpu')
-
-            if save_pred: 
-                all_preds.append(preds_reshape.to('cpu'))
-                all_gt.append(gt_reshape.to('cpu'))
-
-            # SSIM calculation if requested
-            if calculate_ssims:
-                # preds and gt now will have a shape of torch.Size([batch_size, 16, 32, 32])
-                # preds_reshape = rearrange(preds, 'b c (h w) -> b c h w', h=32)
-                preds_reshape[preds_reshape < 0] = 0
-                # gt_reshape = rearrange(gt, 'b c (h w) -> b c h w', h=32)
-                ssims_ = ssim(gt_reshape, preds_reshape, reduction='none')
-                ssims_ = ssims_.to('cpu')
-                ssims[s:e] = ssims_
-                 
-            del batch
-            if device != 'cpu':
-                gc.collect()
-                torch.cuda.empty_cache()
-
-        if save_pred:
-            combined_preds = torch.cat(all_preds, dim=0)
-            combined_gt = torch.cat(all_gt, dim=0)
-    
-            print(f"-- Saving predicted images with shape {list(combined_preds.shape)} to {os.path.join(pred_img_dir_path, f'pred_images_size={combined_preds.shape[0]}_{size}panel.pt')}......")
-    
-            print(f"-- Saving ground truth images with shape {list(combined_gt.shape)} to {os.path.join(pred_img_dir_path, f'gt_images_size={combined_gt.shape[0]}_{size}panel.pt')}......")
-    
-            torch.save(combined_preds, os.path.join(pred_img_dir_path, f'pred_images_size={combined_preds.shape[0]}_{size}panel.pt'))
-            torch.save(combined_gt, os.path.join(pred_img_dir_path, f'gt_images_size={combined_gt.shape[0]}_{size}panel.pt'))
-        
-    return (mints, pmints, mints_combined, pmints_combined, ssims, panel, masked_ch_idx_list, current_marker_panel, ordered_markers_to_impute)
+    return mints, pmints, ssims
 
 
-def plot_intensities(mints, pmints, masked_ch_idx, ch2stain):
-    mints, pmints = mints, pmints
+def get_spearman(mints, pmints):
     if mints.shape[1] == 1:
-        stain_corrs = spearman(pmints.squeeze(), mints.squeeze())
-        stain_corrs = stain_corrs.unsqueeze(0)
+        corrs_per_marker = spearman(pmints.squeeze(), mints.squeeze())
+        corrs_per_marker = corrs_per_marker.unsqueeze(0)
     else:
-        stain_corrs = spearman(pmints, mints)
-    #cls iteratively selected panel
-    fig, ax = plt.subplots(1, len(masked_ch_idx), figsize=(12 * len(masked_ch_idx), 12), layout='tight')
-    for i,a in enumerate(fig.axes):
-
-        a.set_title(f'{ch2stain[masked_ch_idx[i]]}\n(⍴={round(stain_corrs[i].item(),2)})', fontsize=120)
-        x,y = mints[:20000,i].cpu(), pmints[:20000,i].cpu()
-        # Calculate the point density
-        xy = np.vstack([x,y])
-        z = gaussian_kde(xy)(xy)
-        a.scatter(x, y, c=z, s=10)
-        a.set_xticks([])
-        a.set_yticks([])
-        a.plot(np.arange(255), np.arange(255), linestyle='dashed', c='black')
-    plt.show()
-    return stain_corrs
+        corrs_per_marker = spearman(pmints, mints)
+    return corrs_per_marker
+    
--- eval/run_panel_selection.py
+++ eval/run_panel_selection.py
@@ -3,24 +3,29 @@ import sys
 import gc
 import argparse
 import json
+import pickle
 from datetime import datetime
 import torch
+import numpy as np
 from torchmetrics.functional import spearman_corrcoef as spearman
 sys.path.append('../data')
 from data import get_panel_selection_data
 from eval_mae import IF_MAE
-from intensity import get_intensities_run_panel_order
-from process_cedar_biolib_immune import get_channel_info
+from intensity import get_intensities
+#from process_cedar_biolib_immune import get_channel_info
+from process_aced_immune import get_channel_info
 from helper import parse_list, str2bool, format_and_print_args, int_or_string, print_vertical_grid, check_gpu_availability, create_intensity_directories, create_metadata_file, create_panel_order_directories
 
 import warnings
 warnings.filterwarnings("ignore")
 
 
+#find most informative marker first (marker that best predicts n - 1 markers)
 def get_channel_order(max_panel_size, val_loader, model, ch2stain, batch_size):
     
     device = model.device
     top_panel = [0]
+    candidate_scores = []
     for num_masked in reversed(range(1, max_panel_size - len(top_panel))):
         masking_ratio = num_masked/max_panel_size
         model.mae.masking_ratio = masking_ratio
@@ -32,8 +37,45 @@ def get_channel_order(max_panel_size, val_loader, model, ch2stain, batch_size):
             candidate_panel.append(ch_candidate)
             print(f'candidate panel:{[ch2stain[ch] for ch in candidate_panel]}')
 
-            mints, pmints = get_intensities_run_panel_order(model, candidate_panel, val_loader, max_panel_size, batch_size, device=device)
+            mints, pmints, _ = get_intensities(model, candidate_panel, val_loader, ch2stain, device=device)
             
+            if mints.shape[1] == 1:
+                mints = mints.squeeze()
+                pmints = pmints.squeeze()
+            corr = torch.mean(spearman(pmints, mints))
+            print(f'{corr=}')
+            
+            candidate_scores.append((candidate_panel, corr))
+
+            if corr > top_corr:
+                print('found new top panel')
+                top_corr = corr
+                top_ch = ch_candidate
+
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        top_panel.append(top_ch)
+    return top_panel, candidate_scores
+
+
+#find easiest marker first (easiest marker to predict from n - 1 markers)
+def get_channel_order_reversed(max_panel_size, val_loader, model, ch2stain, batch_size):
+    device = model.device
+    top_panel = []
+    for num_masked in range(1,max_panel_size):
+        masking_ratio = num_masked/max_panel_size
+        model.mae.masking_ratio = masking_ratio
+
+        top_corr = -999
+        top_ch = None
+        for ch_candidate in range(max_panel_size):
+            if ch_candidate in top_panel: continue
+            candidate_panel = [i for i in range(max_panel_size) if (i not in top_panel) and (i != ch_candidate)]
+            print(f'candidate panel:{[ch2stain[ch] for ch in candidate_panel]}')
+            print(f'target marker: {ch2stain[ch_candidate]}')
+
+            mints, pmints, _ = get_intensities(model, candidate_panel, val_loader, ch2stain, device=device)
             if mints.shape[1] == 1:
                 mints = mints.squeeze()
                 pmints = pmints.squeeze()
@@ -49,26 +91,102 @@ def get_channel_order(max_panel_size, val_loader, model, ch2stain, batch_size):
             torch.cuda.empty_cache()
 
         top_panel.append(top_ch)
-    return top_panel
+    top_panel.extend([i for i in range(len(ch2stain)) if i not in top_panel])
+    return list(reversed(top_panel))[:-1], None
 
+def get_channel_order_worst_ch(max_panel_size, val_loader, model, ch2stain, batch_size):
+    
+    device = model.device
+    top_panel = [0]
+    candidate_scores = []
+    for num_masked in reversed(range(1, max_panel_size - len(top_panel))):
+        masking_ratio = num_masked/max_panel_size
+        model.mae.masking_ratio = masking_ratio
+        candidate_panel = top_panel.copy()
+        mints, pmints, _ = get_intensities(model, candidate_panel, val_loader, ch2stain, device=device)
+        if mints.shape[1] == 1:
+            mints = mints.squeeze()
+            pmints = pmints.squeeze()
+        corr = spearman(pmints, mints)
+        worst_pred_ch = corr.argmin().item()
+        print(corr.mean())
+        unmasked_chs = [i for i in range(max_panel_size) if i not in top_panel]
+        top_panel.append(unmasked_chs[worst_pred_ch])
+        
 
-def main(run_name, project_path, val_dataset, val_dataset_size, remove_background, ckpt, max_panel_size, gpu_id, batch_size, reverse_channel_order, param_file): 
+        gc.collect()
+        torch.cuda.empty_cache()
+        
+    return top_panel.astype('int').tolist(), None
+
+def get_channel_order_forward_reverse(max_panel_size, val_loader, model, ch2stain, batch_size):
+    device = model.device
+    top_panel = np.ones(max_panel_size) * -999
+    top_panel[0] = 0
+    forward_i = 1
+    backward_i = max_panel_size - 1
+    forward = True
+    while forward_i < backward_i:
+        top_corr = -999
+        top_ch = None
+        for ch_candidate in range(max_panel_size):
+            already_used = [i for i in top_panel if i != -999]
+            if ch_candidate in already_used: continue
+            if forward:
+                candidate_panel = top_panel[:forward_i].astype('int').tolist() + [ch_candidate]
+            else:
+                candidate_panel = top_panel[:forward_i].astype('int').tolist() + [i for i in range(max_panel_size) if (i not in top_panel) and (i != ch_candidate)]
+            print(f'candidate panel:{[ch2stain[ch] for ch in candidate_panel]}')
+            
+            num_masked = max_panel_size - len(candidate_panel)
+            masking_ratio = num_masked/max_panel_size
+            model.mae.masking_ratio = masking_ratio
+            mints, pmints, _ = get_intensities(model, candidate_panel, val_loader, ch2stain, device=device)
+            if mints.shape[1] == 1:
+                mints = mints.squeeze()
+                pmints = pmints.squeeze()
+            corr = torch.mean(spearman(pmints, mints))
+            print(f'{corr=}')
+
+            if corr > top_corr:
+                print('found new top panel')
+                top_corr = corr
+                top_ch = ch_candidate
+            
+            gc.collect()
+            torch.cuda.empty_cache() 
+            
+        if forward:
+            top_panel[forward_i] = top_ch
+            forward_i += 1
+        else:
+            top_panel[backward_i] = top_ch
+            backward_i -= 1
+        print(f' {forward=}, {top_ch=}, {top_corr=}, {top_panel=}')
+        forward = not forward
+        
+    last_marker_idx = np.where(top_panel==-999)
+    top_panel[last_marker_idx] = [i for i in range(max_panel_size) if i not in top_panel][0]
+    return top_panel[:-1].astype('int').tolist(), None
+    
+
+def main(dataset, dataset_size, remove_background, ckpt, max_panel_size, gpu_id, batch_size, reverse, forward_reverse, param_file): 
     
     device = check_gpu_availability(gpu_id)
 
     print("---------------------------------- Collecting Channel Info --------------------------------")
     keep_channels, keep_channels_idx, ch2idx = get_channel_info()
+    
     ch2stain = {i:ch for ch,i in ch2idx.items()}
     print_vertical_grid(ch2stain) # print the marker order 
     print("\n")
-    
 
     try: 
         print("---------------------------------- Loading Validation Dataset --------------------------------")
         # We also need cell location information, that is embedded in the image file, we need to get that information
         # and append it to the feature intensity table. 
-        print("-- Loading testing dataset", val_dataset, "......")
-        val_loader = get_panel_selection_data(val_dataset, batch_size, val_dataset_size, remove_background) 
+        print("-- Loading testing dataset", dataset, "......")
+        val_loader = get_panel_selection_data(dataset, batch_size, dataset_size, remove_background) 
         print("-- Testing dataset loaded successfully! ")
     except Exception as e:
         print("An error occurred while loading the testing dataset:", e)
@@ -97,37 +215,50 @@ def main(run_name, project_path, val_dataset, val_dataset_size, remove_backgroun
 
     print("---------------------------------- Getting Panel Order --------------------------------")
     # Decide which function to call based on reverse_channel_order
-    if reverse_channel_order:
-        top_panel = get_channel_order_reversed(val_loader, model, ch2stain)
+    if reverse:
+        top_panel, candidate_scores = get_channel_order_reversed(max_panel_size, val_loader, model, ch2stain, batch_size)
+    elif forward_reverse:
+        top_panel, candidate_scores = get_channel_order_forward_reverse(max_panel_size, val_loader, model, ch2stain, batch_size)
     else:
-        top_panel = get_channel_order(max_panel_size, val_loader, model, ch2stain, batch_size)
+        top_panel, candidate_scores = get_channel_order(max_panel_size, val_loader, model, ch2stain, batch_size)
 
-    val_dataset_name, panel_order_dir_path = create_panel_order_directories(val_dataset, project_path, ckpt, run_name)
-
-    output_filename = f'{val_dataset_name}_panel_order.txt'
-    output_path = os.path.join(panel_order_dir_path, output_filename)
+    #with open('candidate_panel_scores.pickle', 'wb') as f:
+    #    pickle.dump(candidate_scores, f)
+        
+    if reverse:
+        method_suffix = 'ips-reversed'
+    elif forward_reverse:
+        method_suffix = 'ips-forward-reverse'
+    else:
+        method_suffix = 'ips'
+        
+    output_filename = f"{dataset[0].split('/')[-1]}_{ckpt.split('/')[-3]}_panel_order_{method_suffix}.txt"
+    output_path = os.path.join('orderings', output_filename)
     with open(output_path, 'w') as f:
         f.write(str(top_panel))
     print(f"Panel order output saved to {output_path}")
     print("\n")
 
+    
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run inference with panel reduction model')
-    parser.add_argument('--run_name', type=str, required=True, help='give an identifier to your current inference run')
-    parser.add_argument('--project_path', type=str, default='/home/groups/ChangLab/wangmar/shift-panel-reduction/shift-panel-reduction-main/eval', help='path to the panel reduction project')
-    parser.add_argument("--val_dataset", type=str, nargs='*', required=True, help="Which CRC dataset to use for testing/validation, e.g. CRC05")
-    parser.add_argument("--val_dataset_size", type=int_or_string, default="all", help="The size of the validation dataset to be used for inferencing")
-    parser.add_argument("--remove_background", action='store_true', help="Whether to remove background information, e.g. True")
+    parser.add_argument("--val_dataset", type=str, nargs='*', required=True, help="Which dataset to use for panel selection")
+    parser.add_argument("--val_dataset_size", type=int_or_string, default="all", help="The number of samples to be used for inferencing")
+    parser.add_argument("--remove_background", action='store_true', default=True, help="Whether to remove background information")
     parser.add_argument("--ckpt", type=str, required=True, help="file path to the checkpoint")
-    parser.add_argument("--max_panel_size", type=int, default=17, help="Maximum panel size for Orion-CRC dataset, 17 for no HE and 20 for HE")
+    parser.add_argument("--max_panel_size", type=int, default=17, help="Maximum Number of Markers in Panel")
     parser.add_argument('--gpu_id', type=int, default=None, help='GPU ID to use (if available)')
     parser.add_argument("--batch_size", type=int, default=20000, help="Batch size for processing")
-    parser.add_argument('--reverse_channel_order', action='store_true',
+    parser.add_argument('--reversed', action='store_true',
                         help='If True, use get_channel_order_reversed(), otherwise use get_channel_order()')   
+    parser.add_argument('--forward-reverse', action='store_true',
+                        help='If True, use combo of get_channel_order and get_channel_order_reversed()')   
     parser.add_argument("--param_file", type=str, default=None, help="json file containing model hyperparameters")
-    args = parser.parse_args()
     
+    args = parser.parse_args()
     format_and_print_args(args, parser) # Print the formatted arguments
-    main(args.run_name, args.project_path, args.val_dataset, args.val_dataset_size, args.remove_background, args.ckpt, args.max_panel_size, args.gpu_id, args.batch_size, args.reverse_channel_order, args.param_file)
+    
+    main(dataset=args.val_dataset, dataset_size=args.val_dataset_size, remove_background=args.remove_background, ckpt=args.ckpt, max_panel_size=args.max_panel_size, gpu_id=args.gpu_id, batch_size=args.batch_size, reverse=args.reversed, forward_reverse=args.forward_reverse, param_file=args.param_file)
 
     print("########## Run Panel Order Complete ##########")
