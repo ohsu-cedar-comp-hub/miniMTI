03772085e061d8e3bd118053652890805459b295
govindsa <govindsa@changlab-gpu1.ohsu.edu>
2025-03-24 15:00:37 -0700

    Temporarily saving copied run_training-mvtm.py

--- eval/intensity.py
+++ eval/intensity.py
@@ -6,12 +6,16 @@ import numpy as np
 from tqdm import tqdm
 from einops import rearrange, repeat
 from torchmetrics.functional import structural_similarity_index_measure as ssim, spearman_corrcoef as spearman
+#from accelerate.utils import gather
+#from accelerate.logging import get_logger
+#logger = get_logger(__name__, log_level="DEBUG")
 
-def get_mints(gt, preds, mask, device):
+def get_mints(gt, preds, mask, device, model_type='mvtm'):
     '''calculates mean intensities (mints) and predicted mean intensities (pmints'''
     #reshape to BxCxHxW
-    preds = rearrange(preds, 'b c (h w) -> b c h w', h=32)
-    gt = rearrange(gt, 'b c (h w) -> b c h w', h=32)
+    if model_type != 'mvtm':
+        preds = rearrange(preds, 'b c (h w) -> b c h w', h=32)
+        gt = rearrange(gt, 'b c (h w) -> b c h w', h=32)
 
     #expand dapi mask to number of reconstructed channels
     mask = repeat(mask,'b h w -> b c h w', c=preds.shape[1])
@@ -24,7 +28,7 @@ def get_mints(gt, preds, mask, device):
     return mints, pmints
 
 
-def get_intensities(model, panel, val_loader, ch2stain, calculate_ssims=False, device='cpu'):
+def get_intensities(model, panel, val_loader, ch2stain, calculate_ssims=False, device='cpu', return_meta=False, model_type='mvtm', distributed_state=None):
     '''runs inference with model in batches on GPU and copies mean intensities and predicted mean intensities (and SSIM if specified) back to CPU'''
     max_panel_size = len(ch2stain.keys())
     ordered_markers_to_impute = [ch2stain[idx] for idx in [i for i in range(max_panel_size) if i not in panel]]
@@ -32,7 +36,8 @@ def get_intensities(model, panel, val_loader, ch2stain, calculate_ssims=False, d
     masked_ch_idx_list = [i for i in range(max_panel_size) if i not in panel]
     masked_ch_idx = torch.tensor(masked_ch_idx_list, device=device)
     panel_ch_idx = torch.tensor(panel, device=device)  # This is the panel list, in this above example, this will be tensor([0, 4, 12])
-    model.mae.masking_ratio = (max_panel_size - len(panel)) / max_panel_size
+    if model_type != "mvtm":
+        model.mae.masking_ratio = (max_panel_size - len(panel)) / max_panel_size
 
     print(f"*************** Processing {len(panel)} panel ***************")
     print("-- Current marker panel is:", current_marker_panel)
@@ -40,7 +45,8 @@ def get_intensities(model, panel, val_loader, ch2stain, calculate_ssims=False, d
     print("-- Markers to impute: ", ordered_markers_to_impute)
     print("-- Markers to impute index: ", [i for i in range(max_panel_size) if i not in panel])
     print("-- Calculating SSIM: ", calculate_ssims)
-    print("-- Current MAE masking ratio is: ", model.mae.masking_ratio)
+    if model_type != 'mvtm':
+        print("-- Current MAE masking ratio is: ", model.mae.masking_ratio)
     
     #initiate empty arrays on CPU
     mints = torch.zeros((len(val_loader.dataset), len(masked_ch_idx)), device='cpu')
@@ -52,22 +58,48 @@ def get_intensities(model, panel, val_loader, ch2stain, calculate_ssims=False, d
     batch_size = None
     for i, batch in enumerate(tqdm(val_loader)):
         ims, masks, filepaths = batch
-        ims = ims.to(device)
+        gt = ims.to(device)
         
         #get batch size from first batch
         if batch_size is None: 
-            batch_size = len(ims)
+            batch_size = len(gt)
             
         #run inference on GPU
         with torch.no_grad():
-            panel_patches, gt, preds, _, _ = model.forward((ims, masks, filepaths), masked_patch_idx=masked_ch_idx)
+            if model_type == 'mvtm':
+                '''
+                all_preds = []
+                with distributed_state.split_between_processes(gt, apply_padding=True) as in_:
+                    out, token_ids, labels = model.forward(in_, masked_ch_idx=masked_ch_idx)
+                    preds = model.decode(token_ids, labels, out['logits'], len(in_))
+                    all_preds.extend(preds)
+                
+                all_preds = gather(all_preds)
+                preds = torch.stack(all_preds).reshape(gt.shape)
+                '''
+                out,token_ids,labels = model.predict(gt, masked_ch_idx=masked_ch_idx)
+                preds = model.detokenize(token_ids, batch_size=gt.shape[0]).reshape(gt.shape)
+                #out, token_ids, labels = model.forward(gt, masked_ch_idx=masked_ch_idx)
+                #preds = model.decode(token_ids, labels, out['logits'], len(gt))
+                
+            else:
+                panel_patches, gt, preds, _, _ = model.forward((gt, masks, filepaths), masked_patch_idx=masked_ch_idx)
 
         # reshape gt and preds from torch.Size([batch_size, 16, 1024]) to torch.Size([batch_size, 16, 32, 32])
-        preds_reshape = rearrange(preds, 'b c (h w) -> b c h w', h=32)
-        gt_reshape = rearrange(gt, 'b c (h w) -> b c h w', h=32)
+        if model_type != 'mvtm':
+            preds_reshape = rearrange(preds, 'b c (h w) -> b c h w', h=32)
+            gt_reshape = rearrange(gt, 'b c (h w) -> b c h w', h=32)
 
+            
+        if model_type == 'mvtm':
+            preds = (preds + 1) * 127.5
+            gt = (gt + 1) * 127.5
         #get mean intensities
         mints_, pmints_ = get_mints(gt, preds, masks, device) # without panel patches 
+        
+        if model_type == 'mvtm':
+            mints_ = mints_[:, masked_ch_idx]
+            pmints_ = pmints_[:, masked_ch_idx]
 
         #copy back to CPU
         s = i * batch_size
@@ -88,6 +120,8 @@ def get_intensities(model, panel, val_loader, ch2stain, calculate_ssims=False, d
             gc.collect()
             torch.cuda.empty_cache()
 
+    if return_meta:
+        return mints, pmints, ssims, filepaths
     return mints, pmints, ssims
 
 
